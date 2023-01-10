5c91f6112a179c4da458b284a59260d1411eb2c1
Rodrigo Lucena <rodrigo@rslucena.com.br>
2023-01-10 18:19:24 -0300

    add PHP to framework
    PHP addition to framework.
    
    Port:
    - "1215:1215" #MULTITHREAD
    - "9000:9000" #SINGLETHREAD
    
    Install:
    - Utils
    - Vim
    - Git
    - Supervisor
    - Opcache
    - PDO
    - Swoole
    - Composer

---
+++ application/App/multithread.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+$http = new Swoole\Http\Server("0.0.0.0", 1215);
+
+$http->on('start', function ($server) {
+    echo "Server started at http://0.0.0.0:1215\n";
+});
+
+$http->on("request", function (Swoole\Http\Request $request, Swoole\Http\Response $response) {
+
+    $response->end("OK");
+
+});
+
+$http->start();
