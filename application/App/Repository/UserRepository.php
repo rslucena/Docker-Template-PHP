d5e7a6b8f7dc121339bf1529fee20ff165154756
Rodrigo Lucena <rodrigo@rslucena.com.br>
2023-01-12 20:32:19 -0300

    Default PSR0 - PSR4
    Only to maintain the pattern of the suggested structure. I recommend each one to use in the way that pleases the most.

--- application/App/Repository/UserRepository.php
+++ application/App/Repository/UserRepository.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Repository;
+namespace Root\Application\Repository;
 
 class UserRepository
 {
--- application/App/singlethread.php
+++ application/App/singlethread.php
@@ -2,13 +2,16 @@
 declare(strict_types=1);
 
 use Root\Application\{
+    Controller\UserController,
     Provider\DotEnvProvider,
-    Service\RedisService
-};
+    Repository\UserRepository,
+    Service\RedisService};
 
 require "../Vendor/autoload.php";
 
 new DotEnvProvider(realpath(__DIR__ . "/../"));
+
+#---
 
 $Redis = new RedisService();
 
@@ -16,5 +19,11 @@ var_dump($Redis->set('demo', 'demo'));
 var_dump($Redis->get('demo'));
 var_dump($Redis->del('demo'));
 var_dump($Redis->exists('demo'));
+
+#---
+
+$UserRepository = new UserRepository();
+$UserController = new UserController();
+
 
 die();
