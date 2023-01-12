05c683970be4da1b9622844af4cd3240ad0aa3a6
Rodrigo Lucena <rodrigo@rslucena.com.br>
2023-01-12 20:23:26 -0300

    Reading .env files
    Dynamic creation of various environments. Reading X numbers of .env files. Addition and validation. Overwrite prevention and removal of native functions.

--- application/App/Provider/DotEnvProvider.php
+++ application/App/Provider/DotEnvProvider.php
@@ -1,13 +1,45 @@
 <?php
 
-namespace Provider;
+namespace Root\Application\Provider;
+
+use InvalidArgumentException;
+use RuntimeException;
 
 class DotEnvProvider
 {
 
-    public function example() : string
+    public function __construct(string $Path)
     {
-        return 'DotEnvProvider';
+        $Path .= '/.env';
+
+        if (!file_exists($Path)) {
+            throw new InvalidArgumentException(sprintf('%s does not exist', $Path));
+        }
+
+        if (!is_readable($Path)) {
+            throw new RuntimeException(sprintf('%s file is not readable', $Path));
+        }
+
+        $Lines = file($Path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+        foreach ($Lines ?? [] as $line) {
+            if (str_starts_with(trim($line), '#')) {
+                continue;
+            }
+
+            $variable = explode('=', $line, 2);
+
+            $name = trim($variable[0] ?? "");
+            $value = trim($variable[1] ?? "");
+
+            if (empty($name) || empty($value)) {
+                continue;
+            }
+
+            if (!array_key_exists($name, $_SERVER) && !array_key_exists($name, $_ENV)) {
+                putenv(sprintf('%s=%s', $name, $value));
+            }
+        }
     }
 
 }
