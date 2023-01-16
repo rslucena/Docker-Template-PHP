1fbe24f2e001543c90b1570171590b1e2ed435b4
Rodrigo Lucena <rodrigo@rslucena.com.br>
2023-01-16 20:56:48 -0300

    Reference corrections
    Correction of service and class references.

--- application/App/singlethread.php
+++ application/App/singlethread.php
@@ -1,29 +1,28 @@
 <?php
+
 declare(strict_types=1);
 
 use Root\Application\{
-    Controller\UserController,
-    Provider\DotEnvProvider,
-    Repository\UserRepository,
-    Service\RedisService};
+    Providers\DotEnvProvider,
+    Services\RedisService,
+    Controllers\UserController
+};
 
 require "../Vendor/autoload.php";
 
 new DotEnvProvider(realpath(__DIR__ . "/../"));
 
-#---
-
 $Redis = new RedisService();
 
-var_dump($Redis->set('demo', 'demo'));
-var_dump($Redis->get('demo'));
-var_dump($Redis->del('demo'));
-var_dump($Redis->exists('demo'));
+$Redis->get('test');
+$Redis->set('test', []);
+$Redis->del('test');
+$Redis->exists('test');
 
-#---
+$User = new UserController();
+$User->create(['test usr']);
+$User->find(1);
+$User->update(1, ['usr']);
+$User->delete(1);
 
-$UserRepository = new UserRepository();
-$UserController = new UserController();
 
-
-die();
