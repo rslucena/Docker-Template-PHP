9a36b8fef5749fbc1dd92526c6dea7eb4ded5536
Rodrigo Lucena <rodrigo@rslucena.com.br>
2023-01-17 22:28:23 -0300

    Correction in the example table data
    Just a change in the example table creation parameters

--- .docker/mysql/scripts/init.sql
+++ .docker/mysql/scripts/init.sql
@@ -1,8 +1,8 @@
-CREATE TABLE usr (
+CREATE TABLE user (
      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-     first_name VARCHAR(255) NOT NULL,
-     last_name VARCHAR(255) NOT NULL,
+     first_name VARCHAR(100) NOT NULL,
+     last_name VARCHAR(100) NOT NULL,
      email VARCHAR(255) NOT NULL UNIQUE,
-     password VARCHAR(255) NOT NULL,
+     password VARCHAR(20) NOT NULL,
      created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
