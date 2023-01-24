183ddc1d3755acd5172b7664ebb6e3aec24affca
Rodrigo Lucena <rodrigo@rslucena.com.br>
2023-01-23 21:57:27 -0300

    New Request Middleware
    Used to standardize the entry of information into global variables. Also limit the behavior to some read-only variables and others to allow access.

---
+++ application/App/Middlewares/RequestMiddleware.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Middlewares;
+
+use Exception;
+
+/**
+ * @method get(mixed $arguments = [])
+ * @method post(mixed $arguments = [])
+ * @method files(mixed $arguments = [])
+ * @method cookie()
+ * @method server()
+ */
+class RequestMiddleware
+{
+
+    private array $GetMethods = [
+        'php://input',
+        'get',
+        'post',
+        'files',
+        'server',
+        'cookie',
+        'headers'
+    ];
+
+    private array $SetMethods = ['get', 'post', 'files'];
+
+    /**
+     * Access php://input arrays
+     *
+     * @return array
+     * @throws \Exception
+     */
+    public function phpInput(): array
+    {
+        $Json = file_get_contents('php://input');
+        $Content = json_decode($Json, true);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new Exception('Invalid JSON data');
+        }
+
+        return $Content;
+    }
+
+    /**
+     * Magic method to access superglobal arrays
+     *
+     * @param string $name
+     * @param array $arguments
+     *
+     * @return array
+     */
+    public function __call(string $name, array $arguments): array
+    {
+        if (in_array($name, $this->GetMethods) === false) {
+            return [];
+        }
+
+        if (!empty($arguments) && in_array($name, $this->SetMethods)) {
+            $this->set($name, $arguments[0]);
+        }
+
+        return $GLOBALS['_' . strtoupper($name)];
+    }
+
+    /**
+     * Set values in superglobal arrays
+     *
+     * @param string $name
+     * @param mixed $value
+     */
+    private function set(string $name, mixed $value): void
+    {
+        $GLOBALS['_' . strtoupper($name)] = $value;
+    }
+
+}
