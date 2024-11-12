3fc3502f48742986b0d4a80e612bda9005337ef8
Rodrigo Lucena <rodrigo@rslucena.com.br>
2024-11-12 19:44:14 -0300

    Change port multithread for 3000

--- application/App/multithread.php
+++ application/App/multithread.php
@@ -2,16 +2,14 @@
 
 declare(strict_types=1);
 
-$http = new Swoole\Http\Server("0.0.0.0", 1215);
+$http = new Swoole\Http\Server("0.0.0.0", 3000);
 
 $http->on('start', function ($server) {
-    echo "Server started at http://0.0.0.0:1215\n";
+    echo "Server started at http://0.0.0.0:3000\n";
 });
 
 $http->on("request", function (Swoole\Http\Request $request, Swoole\Http\Response $response) {
-
     $response->end("OK");
-
 });
 
 $http->start();
