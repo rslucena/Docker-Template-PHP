e7b412c9529df90c9696733050b09afcf1c13b53
Rodrigo Lucena <rodrigo@rslucena.com.br>
2024-11-12 19:49:17 -0300

    Fix: Align routes and optimize imports for middlewares to work correctly

--- application/App/singlethread.php
+++ application/App/singlethread.php
@@ -4,10 +4,9 @@ declare(strict_types=1);
 
 use App\Bootstrap\{DotEnvBootstrap, RouterBootstrap, SettingsBootstrap};
 
-
 require "../Vendor/autoload.php";
 
-DotEnvBootstrap::load(__DIR__);
+DotEnvBootstrap::load(dirname(__DIR__, 2));
 DotEnvBootstrap::definePath(__DIR__);
 
 SettingsBootstrap::load();
@@ -22,17 +21,15 @@ $Routes->get('/exemple', function () {
 $Routes->post('/login', 'App\Controllers\UserController::Auth');
 
 #Middleware
-$Routes->get('/users', 'App\Controllers\UserController::viewProfile');
-$Routes->get('/users/{id}', 'App\Controllers\UserController::viewProfile', ['App\Middlewares\AuthenticationMiddleware']);
-$Routes->put('/users/{id}', 'App\Controllers\UserController::updateProfile', ['App\Middlewares\AuthenticationMiddleware']);
-$Routes->delete('/users/{id}', 'App\Controllers\UserController::deleteUser', ['App\Middlewares\AuthenticationMiddleware']);
+$Routes->get('/users/all', 'App\Controllers\UserController::ByParams', ['App\Middlewares\AuthenticationMiddleware']);
+$Routes->get('/users/{id}', 'App\Controllers\UserController::ByID', ['App\Middlewares\AuthenticationMiddleware']);
+$Routes->put('/users/{id}', 'App\Controllers\UserController::UpdateByID', ['App\Middlewares\AuthenticationMiddleware']);
+$Routes->delete('/users/{id}', 'App\Controllers\UserController::DeleteByID', ['App\Middlewares\AuthenticationMiddleware']);
 $Routes->options('/users/{id}', 'App\Controllers\UserController::getAllowedMethods', ['App\Middlewares\AuthenticationMiddleware']);
 
 try {
-
     $Response = $Routes->match($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
     $Response->end();
-
 } catch (Exception $exception) {
     die($exception->getMessage());
 }
